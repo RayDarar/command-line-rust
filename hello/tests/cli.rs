0275c1cc74ccf429eba4fa4b2ec47e1ccac850e4
RayDarar <dfqgth400@gmail.com>
2023-12-22 22:32:29 +0600

    feat(hello): simplify tests

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -4,9 +4,5 @@ use assert_cmd::Command;
 fn it_works() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
 
-    cmd.assert().success();
-
-    let output = cmd.output().unwrap();
-    
-    assert_eq!(output.stdout, b"Hello, world!\n")
+    cmd.assert().success().stdout("Hello, world!\n");
 }
