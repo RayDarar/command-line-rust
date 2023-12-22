f9217418ab3770f7b0bc27caf42f6df72d9b41d9
RayDarar <dfqgth400@gmail.com>
2023-12-22 22:28:49 +0600

    feat(hello): update tests

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -5,4 +5,8 @@ fn it_works() {
     let mut cmd = Command::cargo_bin("hello").unwrap();
 
     cmd.assert().success();
+
+    let output = cmd.output().unwrap();
+    
+    assert_eq!(output.stdout, b"Hello, world!\n")
 }
